92130be5cdcfccfd97a8833d557ab77491f544d3
Richard McAlister <rick-mcalister@msn.com>
2020-12-25 15:11:40 -1000

    Add project files.

---
+++ TSXConfigurationReader/FormConfigurationReader.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TSXConfigurationReader
+{
+    partial class FormConfigurationReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button OpenButton;
+            System.Windows.Forms.Button ParseButton;
+            System.Windows.Forms.Button SearchButton;
+            this.INIopenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.ConfigFileTextBox = new System.Windows.Forms.TextBox();
+            this.cfgTreeView = new System.Windows.Forms.TreeView();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            OpenButton = new System.Windows.Forms.Button();
+            ParseButton = new System.Windows.Forms.Button();
+            SearchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            OpenButton.Location = new System.Drawing.Point(12, 12);
+            OpenButton.Name = "OpenButton";
+            OpenButton.Size = new System.Drawing.Size(75, 23);
+            OpenButton.TabIndex = 0;
+            OpenButton.Text = "Open";
+            OpenButton.UseVisualStyleBackColor = true;
+            OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // ParseButton
+            // 
+            ParseButton.Location = new System.Drawing.Point(516, 680);
+            ParseButton.Name = "ParseButton";
+            ParseButton.Size = new System.Drawing.Size(63, 23);
+            ParseButton.TabIndex = 4;
+            ParseButton.Text = "Collapse";
+            ParseButton.UseVisualStyleBackColor = true;
+            ParseButton.Click += new System.EventHandler(this.ParseButton_Click);
+            // 
+            // SearchButton
+            // 
+            SearchButton.Location = new System.Drawing.Point(415, 680);
+            SearchButton.Name = "SearchButton";
+            SearchButton.Size = new System.Drawing.Size(63, 23);
+            SearchButton.TabIndex = 6;
+            SearchButton.Text = "Search";
+            SearchButton.UseVisualStyleBackColor = true;
+            SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // INIopenFileDialog
+            // 
+            this.INIopenFileDialog.Filter = "Config Files (.ini)|*.ini|All Files (*.*)|*.*\"";
+            // 
+            // ConfigFileTextBox
+            // 
+            this.ConfigFileTextBox.Location = new System.Drawing.Point(93, 15);
+            this.ConfigFileTextBox.Name = "ConfigFileTextBox";
+            this.ConfigFileTextBox.Size = new System.Drawing.Size(486, 20);
+            this.ConfigFileTextBox.TabIndex = 1;
+            // 
+            // cfgTreeView
+            // 
+            this.cfgTreeView.Location = new System.Drawing.Point(12, 53);
+            this.cfgTreeView.Name = "cfgTreeView";
+            this.cfgTreeView.Size = new System.Drawing.Size(567, 607);
+            this.cfgTreeView.TabIndex = 2;
+            this.cfgTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.cfgTreeView_AfterSelect);
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(12, 682);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(397, 20);
+            this.SearchTextBox.TabIndex = 5;
+            this.SearchTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SearchTextBox_KeyDown);
+            // 
+            // FormConfigurationReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(595, 722);
+            this.Controls.Add(SearchButton);
+            this.Controls.Add(this.SearchTextBox);
+            this.Controls.Add(ParseButton);
+            this.Controls.Add(this.cfgTreeView);
+            this.Controls.Add(this.ConfigFileTextBox);
+            this.Controls.Add(OpenButton);
+            this.Name = "FormConfigurationReader";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog INIopenFileDialog;
+        private System.Windows.Forms.TextBox ConfigFileTextBox;
+        private System.Windows.Forms.TreeView cfgTreeView;
+        private System.Windows.Forms.TextBox SearchTextBox;
+    }
+}
+
---
+++ TSXConfigurationReader/FormConfigurationReader.cs
@@ -0,0 +1,158 @@
+﻿using IniParser;
+using IniParser.Model;
+using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TSXConfigurationReader
+{
+    public partial class FormConfigurationReader : Form
+    {
+        public FormConfigurationReader()
+        {
+            InitializeComponent();
+            // Acquire the version information and put it in the form header
+            try { this.Text = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString(); }
+            catch { this.Text = " in Debug"; } //probably in debug, no version info available
+            this.Text = "TSX Configuration Reader Ver " + this.Text;
+        }
+
+        IniData pData;
+
+        #region form events
+
+        private void OpenButton_Click(object sender, EventArgs e)
+
+        {
+            char[] bracketchars = new char[] { '[', ']' };
+            int cN = 0;
+            string section = "";
+
+            List<string> cfgRaw = new List<string>();
+            if (INIopenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ConfigFileTextBox.Text = INIopenFileDialog.FileName;
+                //Read in the file to a string list by line
+                var parser = new FileIniDataParser();
+                pData = parser.ReadFile(ConfigFileTextBox.Text);
+                cfgRaw = File.ReadAllLines(ConfigFileTextBox.Text).ToList();
+                cfgTreeView.Nodes.Clear();
+                foreach (string line in cfgRaw)
+                {
+                    if (line.Contains("["))
+                    {
+                        section = line.Trim(bracketchars);
+                        cN = AddMainNode(section);
+                    }
+                    else if (line != "")
+                    {
+                        if (line.Contains("Selection"))
+                        {
+                            string selectNode = "Selection=" + ParseSelection(section);
+                            AddLeafNode(cN, selectNode);
+                        }
+                        else AddLeafNode(cN, StretchEquals(line));
+                    }
+                }
+                cfgTreeView.Show();
+            }
+            return;
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            RunSearch(SearchTextBox.Text);
+            return;
+        }
+
+        private void ParseButton_Click(object sender, EventArgs e)
+        {
+            cfgTreeView.CollapseAll();
+            foreach (TreeNode node in cfgTreeView.Nodes)
+            {
+                foreach (TreeNode subnode in node.Nodes) subnode.ForeColor = Color.Black;
+            }
+            cfgTreeView.Show();
+            SearchTextBox.Clear();
+            return;
+        }
+
+        private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13) RunSearch(SearchTextBox.Text);
+            return;
+        }
+
+        private void cfgTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //Add selection (less assigned value) to favorites list
+            //char [] sChar = new char[] { '='};
+            string[] pickLine = cfgTreeView.SelectedNode.Text.Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+            string pick = pickLine[0];
+            Show();
+        }
+
+        #endregion
+
+        private int AddMainNode(string section)
+        {
+            TreeNode cNode = cfgTreeView.Nodes.Add(section);
+            return cfgTreeView.Nodes.IndexOf(cNode);
+        }
+
+        private int AddLeafNode(int main, string leaf)
+        {
+            TreeNode cNode = cfgTreeView.Nodes[main].Nodes.Add(leaf);
+            return cfgTreeView.Nodes.IndexOf(cNode);
+        }
+
+        private string ParseSelection(string section)
+        {
+            //Figures out and returns the item entry selected in a Selection Line
+            string pOut = pData[section]["Selection"];
+            if (pOut != null)
+            {
+                string[] sItems = pOut.Split('|');
+                if (sItems[0].Length > 1) sItems[0] = sItems[0].Substring(sItems[0].Length - 1, 1);
+                int sIndx = Convert.ToInt32(sItems[0]);
+                string selected = sItems[sIndx + 1];
+                return selected;
+            }
+            else return "";
+        }
+
+        private string StretchEquals(string line)
+        {
+            return (line.Replace("=", " = "));
+        }
+
+        private void RunSearch(string sText)
+        {
+            cfgTreeView.CollapseAll();
+            cfgTreeView.Show();
+            foreach (TreeNode node in cfgTreeView.Nodes)
+            {
+                foreach (TreeNode subnode in node.Nodes)
+                {
+                    if (subnode.Text.ToLower().Contains(sText.ToLower()))
+                    {
+                        cfgTreeView.SelectedNode = subnode;
+                        subnode.ForeColor = Color.Red;
+                    }
+                    else subnode.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private string SearchEntries(string section, string subentry)
+        {
+            return pData[section]["*" + subentry + "*"];
+        }
+
+    }
+}
+
